3ba0d6d683aa4f8d657a33d72d4fcdad8cdc2dbd
Nick Chelmis <nikos.helmis@gmail.com>
2018-04-09 17:40:59 +0300

    Prepare Problem9

---
+++ Problems/Problem9.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace Problems
+{
+    /// <summary>
+    /// Problem 9: Special Pythagorean triplet
+    /// Source: https://projecteuler.net/problem=9
+    /// A Pythagorean triplet is a set of three natural numbers, a &lt; b &lt; c, for which,
+    /// a^2 + b^2 = c^2
+    /// For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+    /// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+    /// Find the product abc.
+    /// </summary>
+    public static class Problem9
+    {
+        public static Answer Solution1()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            int a  = 0, b = 0, c = 0;
+            sw.Stop();
+            return new Answer
+            {
+                Result = 0,
+                TimeTaken = sw.Elapsed.TotalMilliseconds,
+                ExtraInfo = $"{a}, {b}, {c}"
+            };
+        }
+    }
+}
--- TestProblemsConsole/Program.cs
+++ TestProblemsConsole/Program.cs
@@ -53,6 +53,10 @@ namespace TestProblemsConsole
             res = Problem8.Solution1();
             Console.WriteLine($"Problem8 first solution result = {res.Result} found in {res.TimeTaken} ms. Digits = {res.ExtraInfo}");
 
+            //Problem 9
+            res = Problem9.Solution1();
+            Console.WriteLine($"Problem9 first solution result = {res.Result} found in {res.TimeTaken} ms. Digits = {res.ExtraInfo}");
+
             Console.ReadKey();
         }
     }
