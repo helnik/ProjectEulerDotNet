78c541a0fc79be619cdcd7f36d57757434cc6592
Nick Chelmis <nikos.helmis@gmail.com>
2018-04-13 23:42:16 +0300

    Fix bug in checking for prime number

--- Problems/Problem7.cs
+++ Problems/Problem7.cs
@@ -34,8 +34,9 @@ namespace Problems
 
         private static bool IsPrime(long number)
         {
-            if ((number <= 1) || (number % 2 == 0)) return false;
+            if (number <= 1) return false;
             if (number == 2) return true;
+            if (number % 2 == 0) return false;
             long i = 3;
             while (i * i <= number)
             {
